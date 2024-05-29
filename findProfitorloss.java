bb2d88d48c9f3a561c09fefa874c1a93af62d5e7
priti-yenare <159990423+priti-yenare@users.noreply.github.com>
2024-05-29 16:42:22 +0530

    Add files via upload

---
+++ findProfitorloss.java
@@ -0,0 +1,32 @@
+class findProfitorloss 
+{
+	public static void main(String[] args) 
+	{
+		int sp=800;
+		int cp=400;
+		int total;
+		if(sp>cp)
+		{
+		total=sp-cp;
+		System.out.println("i have profit"+total);
+		}
+		else if(cp>sp)
+		{
+		total=cp-sp;
+		System.out.println("i have loss"+total);
+		} else
+		{
+          System.out.println("i have't profit or loss");
+		}
+
+
+	
+
+
+
+
+
+			
+
+	}
+}
