2fe0f2866e8c84237cd49abfe3941a37aeab71a5
priti-yenare <159990423+priti-yenare@users.noreply.github.com>
2024-05-29 16:39:50 +0530

    Add files via upload

---
+++ findPower.java
@@ -0,0 +1,15 @@
+class findPower
+{
+	public static void main(String[] args) 
+	{
+		int num=2;
+		int power=3;
+		int mul=1;
+		for(int i=1;i<=power;i++)
+		{
+			mul=mul*num;
+		}
+		System.out.print(mul);
+		
+	}
+}
