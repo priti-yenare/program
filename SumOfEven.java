82cd5467078781044f3b89098851746a4589284a
priti-yenare <159990423+priti-yenare@users.noreply.github.com>
2024-05-31 10:08:39 +0530

    Add files via upload

---
+++ SumOfEven.java
@@ -0,0 +1,19 @@
+class  SumOfEven
+{
+    public static void main(String[]args)
+    { 
+        int n=100;
+        int sum=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(i%2==0)
+            {
+               sum=sum+i;
+            }
+        }
+        System.out.println(sum);
+    
+    }
+
+
+    }
